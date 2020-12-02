86ecc9ac78e3feb76170015b991ad5c238cf6600
burnsred94 <67732974+burnsred94@users.noreply.github.com>
2020-12-02 15:55:52 +0300

    feat:add footer block elements

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("btn__icon-fill__left"),
+    color = document.getElementById("icon__fill-left");
+
+
+btn.addEventListener('click', function () {
+    console.log('click');
+    color.style.fill = "green"
+    
+})
